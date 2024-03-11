b38b1accb03bbf78ba8171ceb875ec8ed7215ddf
SpaceVikingEik <37214305+SpaceVikingEik@users.noreply.github.com>
2024-03-11 13:26:58 +0100

    working?

--- DGG.py
+++ DGG.py
@@ -5,6 +5,7 @@ map = input()
 mapArray = []
 vertexArray = []
 edgeDict = {}
+hasSeen = set()
 
 for i in range(height):
     line = list(input())
@@ -19,49 +20,137 @@ for x in range(height):
 
 
 def check_vertex(x,y):
-    if(x != 0 and y!= width-1 and x != height and y != 0):
+    if x == 0 and y == 0:
+        check_set_right(x,y)
+        check_set_down(x,y)
+        if(mapArray[x][y] == '.'):
+            if mapArray[x+1][y] == '.':
+                create_edge_down(x,y)
+            if mapArray[x][y+1] == '.':
+                create_edge_right(x,y)
+                
+    elif x == 0 and y == width-1:
+        check_set_left(x,y)
+        check_set_down(x,y)
+        if(mapArray[x][y] == '.'):
+                if mapArray[x+1][y] == '.':
+                    create_edge_down(x,y)
+                if mapArray[x][y-1] == '.':
+                    create_edge_left(x,y)
+    elif x == height-1 and y == 0:
+        check_set_up(x,y)
+        check_set_right(x,y)
+        if(mapArray[x][y] == '.'):
+                if mapArray[x-1][y] == '.':
+                    create_edge_up(x,y)
+                if mapArray[x][y+1] == '.':
+                    create_edge_right(x,y)
+    elif x == height-1 and y == width-1:
+        check_set_up(x,y)
+        check_set_left(x,y)
+        if(mapArray[x][y] == '.'):
+                if mapArray[x-1][y] == '.':
+                    create_edge_up(x,y)
+                if mapArray[x][y-1] == '.':
+                    create_edge_left(x,y)
+    elif x == 0:
+        check_set_right(x,y)
+        check_set_left(x,y)
+        check_set_down(x,y)
+        if(mapArray[x][y] == '.'):
+                if mapArray[x][y+1] == '.':
+                    create_edge_right(x,y)
+                if mapArray[x][y-1] == '.':
+                    create_edge_left(x,y)
+                if mapArray[x+1][y] == '.':
+                    create_edge_down(x,y)
+    elif x == height-1:
+        check_set_right(x,y)
+        check_set_left(x,y)
+        check_set_up(x,y)        
+        if(mapArray[x][y] == '.'):
+                if mapArray[x][y+1] == '.':
+                    create_edge_right(x,y)
+                if mapArray[x][y-1] == '.':
+                    create_edge_left(x,y)
+                if mapArray[x-1][y] == '.':
+                    create_edge_up(x,y)
+                    
+    elif y == 0:
+        check_set_down(x,y)
+        check_set_right(x,y)
+        check_set_up(x,y)
+        if(mapArray[x][y] == '.'):
+                if mapArray[x+1][y] == '.':
+                    create_edge_down(x,y)
+                if mapArray[x-1][y] == '.':
+                    create_edge_up(x,y)
+                if mapArray[x][y+1] == '.':
+                    create_edge_right(x,y)
+    elif y == width-1:
+        check_set_down(x,y)
+        check_set_left(x,y)
+        check_set_up(x,y)
+        if(mapArray[x][y] == '.'):
+                if mapArray[x+1][y] == '.':
+                    create_edge_down(x,y)
+                if mapArray[x-1][y] == '.':
+                    create_edge_up(x,y)
+                if mapArray[x][y-1] == '.':
+                    create_edge_left(x,y)
+
+    elif(x != 0 and y!= width-1 and x != height-1 and y != 0):
         print(x,y)
-        if (mapArray[x][y] == '.'):
-            if(mapArray[x+1][y] == '.'):
-                
-                if(edgeDict[(x,y)].count((x+1,y)) == 0):
+        check_set_right(x,y)
+        check_set_left(x,y)
+        check_set_down(x,y)
+        check_set_up(x,y)
             
-                    vertexStack.append((x+1,y))
-                    edgeDict[(x,y)].append(((x+1,y)))
-                    edgeDict[(x+1,y)].append(((x,y)))
-
-
+        if (mapArray[x][y] == '.'):
+            if(mapArray[x+1][y] == '.'):    
+                create_edge_down(x,y)
             if(mapArray[x-1][y] == '.'):
-                
-       
-                if(edgeDict[(x,y)].count((x-1,y)) == 0):
-                    vertexStack.append((x-1,y))
-                    edgeDict[(x,y)].append(((x-1,y)))
-                    edgeDict[(x-1,y)].append(((x,y)))
-
-
+                create_edge_up(x,y)
             if(mapArray[x][y+1] == '.'):
-                
- 
-                if(edgeDict[(x,y)].count((x,y+1)) == 0):
-                    vertexStack.append((x,y+1))
-                    edgeDict[(x,y)].append(((x,y+1)))
-                    edgeDict[(x,y+1)].append(((x,y)))
-
-
+                create_edge_right(x,y)
             if(mapArray[x][y-1] == '.'):
-                
-  
-                if(edgeDict[(x,y)].count((x,y-1)) == 0):
-                    vertexStack.append((x,y-1))
-                    edgeDict[(x,y)].append(((x,y-1)))
-                    edgeDict[(x,y-1)].append(((x,y)))
-    #else:
-        #vertexStack.append((x+1,y))
-        #vertexStack.append((x-1,y))
-        #vertexStack.append((x,y+1))
-        #vertexStack.append((x,y-1))
+                create_edge_left(x,y)
 
+
+def create_edge_down(x,y):
+    if(edgeDict[(x,y)].count((x+1,y)) == 0):
+        edgeDict[(x,y)].append(((x+1,y)))
+        edgeDict[(x+1,y)].append(((x,y)))
+def create_edge_up(x,y):
+    if(edgeDict[(x,y)].count((x-1,y)) == 0):
+        edgeDict[(x,y)].append(((x-1,y)))
+        edgeDict[(x-1,y)].append(((x,y)))
+def create_edge_right(x,y):
+    if(edgeDict[(x,y)].count((x,y+1)) == 0):
+        edgeDict[(x,y)].append(((x,y+1)))
+        edgeDict[(x,y+1)].append(((x,y)))
+def create_edge_left(x,y):
+    if(edgeDict[(x,y)].count((x,y-1)) == 0):
+        edgeDict[(x,y)].append(((x,y-1)))
+        edgeDict[(x,y-1)].append(((x,y)))
+
+
+def check_set_down(x,y):
+    if((x+1,y) not in hasSeen):
+        vertexStack.append((x+1,y))
+        hasSeen.add((x+1,y))
+def check_set_up(x,y):
+    if((x-1,y) not in hasSeen):
+        vertexStack.append((x-1,y))
+        hasSeen.add((x-1,y))
+def check_set_right(x,y):
+    if((x,y+1) not in hasSeen):
+        vertexStack.append((x,y+1))
+        hasSeen.add((x,y+1))
+def check_set_left(x,y):
+    if((x,y-1) not in hasSeen):
+        vertexStack.append((x,y-1))
+        hasSeen.add((x,y-1))
 
 
 def create_dir_graph():
