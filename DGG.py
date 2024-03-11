376c664fb0521d6b74f0a442dd577476d12ae580
SpaceVikingEik <37214305+SpaceVikingEik@users.noreply.github.com>
2024-03-11 12:01:47 +0100

    gogo

--- DGG.py
+++ DGG.py
@@ -3,6 +3,9 @@ height = int(input().split(" ")[1])
 width = int(input().split(" ")[1])
 map = input()
 mapArray = []
+vertexArray = []
+edgeArray = []
+
 for i in range(height):
     line = list(input())
     mapArray.append(line)
