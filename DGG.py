29b07cb9f0369f3f2ecca7e7e0544d7ae4b0abb3
SpaceVikingEik <37214305+SpaceVikingEik@users.noreply.github.com>
2024-03-11 12:31:39 +0100

    stack-based DGG

--- DGG.py
+++ DGG.py
@@ -4,10 +4,57 @@ width = int(input().split(" ")[1])
 map = input()
 mapArray = []
 vertexArray = []
-edgeArray = []
+edgeDict = {
+    (0,0) : []
+}
 
 for i in range(height):
     line = list(input())
     mapArray.append(line)
 
-print(mapArray)
+vertexStack = []
+vertexStack.append((1,1))
+def check_vertex(x,y):
+    if(x != 0 and y!= width-1 and x != height and y != 0):
+        if (mapArray[x][y] == '.'):
+            if(mapArray[x+1][y] == '.'):
+                vertexStack.append((x+1,y))
+                if(edgeDict[(x,y)].count((x+1,y) == 0)):
+                    edgeDict[(x,y)].append(((x+1,y)))
+                    edgeDict[(x+1,y)].append(((x,y)))
+
+
+            if(mapArray[x-1][y] == '.'):
+                vertexStack.append((x-1,y))
+                if(edgeDict[(x,y)].count((x-1,y) == 0)):
+                    edgeDict[(x,y)].append(((x-1,y)))
+                    edgeDict[(x-1,y)].append(((x,y)))
+
+
+            if(mapArray[x][y+1] == '.'):
+                vertexStack.append((x,y+1))
+                if(edgeDict[(x,y)].count((x,y+1) == 0)):
+                    edgeDict[(x,y)].append(((x,y+1)))
+                    edgeDict[(x,y+1)].append(((x,y)))
+
+
+            if(mapArray[x][y-1] == '.'):
+                vertexStack.append((x,y-1))
+                if(edgeDict[(x,y)].count((x,y-1) == 0)):
+                    edgeDict[(x,y)].append(((x,y-1)))
+                    edgeDict[(x,y-1)].append(((x,y)))
+    else:
+        vertexStack.append((x+1,y))
+        vertexStack.append((x-1,y))
+        vertexStack.append((x,y+1))
+        vertexStack.append((x,y-1))
+
+
+
+def create_dir_graph():
+    while len(vertexStack) != 0:
+        x,y = vertexStack.pop()    
+        check_vertex(x,y)
+
+create_dir_graph()
+print(edgeDict)
