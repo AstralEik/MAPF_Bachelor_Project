32407e237af5fafaa3b6f19ace3e0ae833ed7133
SpaceVikingEik <37214305+SpaceVikingEik@users.noreply.github.com>
2024-03-12 12:06:50 +0100

    first try with simple search

--- STAstar.py
+++ STAstar.py
@@ -1,4 +1,30 @@
 from UDGG import *
 
-hewwo = createGraph()
-print(hewwo)
+UDG = createGraph()
+
+agent = ((0,1),(2,1))
+
+vertexStack = []
+hasSeenSet = set()
+
+def expandVertex(x,y):
+    hasSeenSet.add((x,y))
+    neighbours = UDG.get((x,y))
+    for vertex in range (neighbours):
+        if vertex not in hasSeenSet:
+            vertexStack.append(vertex)
+            
+
+def findPathTo(x1,y1,x2,y2):
+    vertexStack.append((x1,y1))
+    while len(vertexStack) != 0:
+        currentVertex = vertexStack.pop()
+        if(currentVertex == (x2,y2)):
+            hasSeenSet.add(x2,y2)
+            break
+        expandVertex(currentVertex)
+print(UDG)
+
+findPathTo(agent[0],agent[1])
+
+print(hasSeenSet)
