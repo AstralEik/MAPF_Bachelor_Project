0af4ba7a648bbc6edaa9327fa404b17ef87a7af4
eikb <eikb@itu.dk>
2024-04-19 21:49:32 +0200

    lol

--- CBS.py
+++ CBS.py
@@ -7,9 +7,9 @@ open = []
 closed = []
 listOfCbsStates = []
 
-#agents = ([1,0,2,3],[0,1,3,2])
+agents = ([1,0,2,3],[0,1,3,2])
 #agents = ([0,1,2,3],[1,0,3,2])
-agents = ([1,1,1,4],[1,3,1,0])
+# agents = ([1,1,1,4],[1,3,1,0])
 class CBSNode:
     def __init__(self, paths, cost, parent) -> None:
         self.paths = paths
